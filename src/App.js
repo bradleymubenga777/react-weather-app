fd5cc2a1dbad5fd99bbe8b7940568d1090cb1713
Bradley Mubenga <bradleymubenga777@gmail.com>
2020-12-22 22:31:25 +0200

    Using Axios To Fetch Weather API

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import axios from 'axios';
 import './main.css';
+import DisplayWeather from './components/DisplayWeather'
 
 class App extends React.Component
 {  
   state = {
-    coords: {
-      latitude: null,
-      longitude: null
+    coords:{
+      latitude: 0,
+      longitude: 0
+    },
+    data: {
+
     }
   }
   componentDidMount()
@@ -19,14 +23,27 @@ class App extends React.Component
         let newCoords = 
         {
           latitude: position.coords.latitude,
-          longitude: position.coords.latitude
+          longitude: position.coords.longitude
         }
 
         this.setState({coords: newCoords});
 
         //API Call
-        axios.get(`http://api.weatherstack.com/current?access_key=b9387a127f3551dc85b4cb0ea944d14bY&query=${this.state.coords.latitude},${this.state.coords.longitude}`).then(res => {
-          console.log(res);
+        axios.get(`http://api.weatherstack.com/current?access_key=b9387a127f3551dc85b4cb0ea944d14b&query=${this.state.coords.latitude},${this.state.coords.longitude}`).then(res => {
+          let weatherData =
+          {
+            location: res.data.location.name,
+            country: res.data.location.country,
+            region: res.data.location.region,
+            cloudCover: res.data.current.cloudcover,
+            feelsLike: res.data.current.feelslike,
+            temperature: res.data.current.temperature,
+            img: res.data.current.weather_code,
+            weatherDescription: res.data.current.weather_descriptions[0],
+            windSpeed: res.data.current.wind_speed
+          }
+
+          this.setState({data: weatherData})
         })
       })
     }
@@ -41,7 +58,7 @@ class App extends React.Component
   {
     return(
       <div className="App">
-  
+        <DisplayWeather />
       </div>
     );
   }
---
+++ src/components/DisplayWeather.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DisplayWeather() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
