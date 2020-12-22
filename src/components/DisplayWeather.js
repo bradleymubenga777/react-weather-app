a7f710ab2579576e6091b1c15e88170affef0214
Bradley Mubenga <bradleymubenga777@gmail.com>
2020-12-22 23:12:13 +0200

    Using Axios To Get Weather API

--- src/components/DisplayWeather.js
+++ src/components/DisplayWeather.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export default function DisplayWeather() {
+export default function DisplayWeather(props) {
+
     return (
         <div>
-            
+            <p>{props.weatherData.location}</p>
         </div>
     )
 }
