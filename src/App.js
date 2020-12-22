38b6f02eef8024c627d2e6600ed00fd0dba4945f
Bradley Mubenga <bradleymubenga777@gmail.com>
2020-12-22 19:58:40 +0200

    Working On Axios Weather API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import './main.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component
+{  
+  state = {
+    coords: {
+      latitude: null,
+      longitude: null
+    }
+  }
+  componentDidMount()
+  {
+    //get device loation
+    if (navigator.geolocation) //Check if navigation is supported
+    {
+      navigator.geolocation.getCurrentPosition((position) => {
+        let newCoords = 
+        {
+          latitude: position.coords.latitude,
+          longitude: position.coords.latitude
+        }
+
+        this.setState({coords: newCoords});
+
+        //API Call
+        axios.get(`http://api.weatherstack.com/current?access_key=b9387a127f3551dc85b4cb0ea944d14bY&query=${this.state.coords.latitude},${this.state.coords.longitude}`).then(res => {
+          console.log(res);
+        })
+      })
+    }
+
+    else
+    {
+
+    }
+  }
+
+  render()
+  {
+    return(
+      <div className="App">
+  
+      </div>
+    );
+  }
 }
 
 export default App;
